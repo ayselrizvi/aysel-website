a80861c46b24c37bb0284efe74effedc21004c9b
ayselrizvi <ayselrizvi@gmail.com>
2025-02-11 13:56:04 -0600

    Initial commit: Personal Website React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Personal Website</h1>
+      <p>This is built using React and hosted on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
+
